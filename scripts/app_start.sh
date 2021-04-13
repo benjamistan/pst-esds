428dd0ff10e6af7bc3086131cd621c19b6ef33bb
Ben - PST <BenPST@Benjamins-MacBook-Pro.local>
2021-04-13 18:10:14 +0100

    appspec

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 npm install serve
-cd /home/ec2-user/server
 serve -s build -l 3000
--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # verify we can access our webpage successfully
-curl -v --silent localhost:3000 2>&1 | grep Emergency
+curl -v --silent localhost:5000 2>&1 | grep Emergency
