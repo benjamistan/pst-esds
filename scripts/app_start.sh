d899273d2c99d44930f80e8e2faba2b959a95c3c
Ben - PST <BenPST@Benjamins-MacBook-Pro.local>
2021-04-14 16:58:07 +0100

    appspec

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'Starting app...'
-cd app
-pm2 start ./node_modules/react-scripts/scripts/start/js --name "esds" --watch
+cd /app
+pm2 start ./node_modules/react-scripts/scripts/start.js --name "esds" --watch
 pm2 startup
 pm2 save
 pm2 restart all
