77179ef6431aa0190d63c1590227d20e34392aa3
Ben - PST <BenPST@Benjamins-MacBook-Pro.local>
2021-04-14 16:45:09 +0100

    appspec

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,7 @@
 cd /app
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 yum -y install nodejs npm
+echo 'Running npm install...'
+npm install
+npm install --save create-react-app react-scripts
+npm install pm2 -g
--- scripts/install_app.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /app
-echo 'Running npm install...'
-npm install
-npm install --save create-react-app react-scripts
-npm install pm2 -g
