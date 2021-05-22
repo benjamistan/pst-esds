d332e286ec2b4d4c08af3d8f3c07be118db2bcc5
Ben Stanley <ben@benstanley.pro>
2021-05-22 15:46:47 +0200

    fix: updated appspec + scripts

---
+++ scripts/after_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /app
+npm install
+npm install --save react react-dom react-scripts
+npm install pm2 -g
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+
 cd /app
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 yum -y install nodejs npm
-echo 'Running npm install...'
-npm install
-npm install --save create-react-app react-scripts
-npm install pm2 -g
+
