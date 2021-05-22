81d48845c3f7ba2e7c32c773f9533359d72045f1
Ben Stanley <ben@benstanley.pro>
2021-05-22 15:57:38 +0200

    fix: commenting appspec

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cd /app
+echo 'Starting npm install'
 npm install
+echo 'Installing React'
 npm install --save react react-dom react-scripts
+echo 'Installing pm2'
 npm install pm2 -g
