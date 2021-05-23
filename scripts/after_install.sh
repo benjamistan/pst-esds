c2e209e4bca325fbe5bb7fd5e0843ed70497f264
Ben Stanley <ben@benstanley.pro>
2021-05-23 14:53:32 +0200

    fix: working on codedeploy scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 cd /app
-echo 'Starting npm install'
+echo '********************************'
+echo '***   Starting npm install   ***'
+echo '********************************'
 npm install
-echo 'Installing React'
+echo '****************************'
+echo '***   Installing React   ***'
+echo '****************************'
 npm install --save react react-dom react-scripts
-echo 'Installing pm2'
+echo '***************************'
+echo '***   Installing pm2    ***'
+echo '***************************'
 npm install pm2 -g
