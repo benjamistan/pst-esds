614fdb35af2e0683647a449e84abfd82ae2ad4cf
Ben Stanley <ben@benstanley.pro>
2021-05-26 20:38:59 +0200

    feat:add aws amplify

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import Footer from './components/footer';
 import Header from './components/header';
 import FileList from './components/fileList';
 import { Container } from '@material-ui/core';
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
+
+Amplify.configure(awsConfig);
 
 function App() {
 	return (
 		<div className='App'>
 			<Container maxWidth='lg'>
 				<Header />
+				<AmplifySignOut />
 				<FileList />
 				<Footer />
 			</Container>
@@ -15,4 +21,4 @@ function App() {
 	);
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/components/fileList.js
+++ src/components/fileList.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
-import AWS from 'aws-sdk';
 import { Container, Typography, List, ListItem } from '@material-ui/core';
 
-const S3_BUCKET = 'esds-incident-files';
-const REGION = 'eu-west-2';
-
-const bucket = new AWS.S3({
-	params: { Bucket: S3_BUCKET },
-	region: REGION,
-});
-
 const FileList = () => {
+	const AWS = require('aws-sdk');
+
+	AWS.config.getCredentials(function (err) {
+		if (err) console.log(err.stack);
+		else {
+			console.log('Access key:', AWS.config.credentials.accessKeyId);
+			console.log('Region:', AWS.config.region);
+		}
+	});
+
+	const S3_BUCKET = 'esds-incident-files';
+	const REGION = 'eu-west-2';
+
+	const bucket = new AWS.S3({
+		params: { Bucket: S3_BUCKET },
+		region: REGION,
+	});
+
 	const [s3Objects, setS3Objects] = useState(null);
 
 	bucket.listObjects(bucket.params, function (err, data) {
